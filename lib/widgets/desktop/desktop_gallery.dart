f4f18e983a6688aef3ef2179f4638471f7618c34
Erick Namukolo <erickmndev@gmail.com>
2022-09-07 20:37:12 +0200

    minor updates

--- lib/widgets/desktop/desktop_gallery.dart
+++ lib/widgets/desktop/desktop_gallery.dart
@@ -12,7 +12,7 @@ class DesktopGallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 80.0, horizontal: 140.0),
+      padding: const EdgeInsets.symmetric(vertical: 80.0),
       color: Colors.white,
       width: double.infinity,
       child: Column(
@@ -28,7 +28,7 @@ class DesktopGallery extends StatelessWidget {
           ),
           const SizedBox(height: 10.0),
           const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 260.0),
+            padding: EdgeInsets.symmetric(horizontal: 400.0),
             child: Text(
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
               style: kBodyTextStyleGrey,
@@ -37,6 +37,7 @@ class DesktopGallery extends StatelessWidget {
           ),
           const SizedBox(height: 40.0),
           GridView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 140.0),
             shrinkWrap: true,
             itemCount: 6,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -51,6 +52,22 @@ class DesktopGallery extends StatelessWidget {
                 image: DecorationImage(
                   image: AssetImage(SiteData.gallery[index]),
                   fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 80.0),
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              image: DecorationImage(
+                image: const AssetImage(
+                    'assets/images/nathan-dumlao-kDxqbAvEBwI-unsplash.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  const Color(0xff65A234).withOpacity(0.3),
+                  BlendMode.srcOver,
                 ),
               ),
             ),
